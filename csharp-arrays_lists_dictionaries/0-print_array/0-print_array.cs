5fdb593f9991bd9e98629c05bce4a6c7f4f07c33
GasanaJr <gasanadj2003@gmail.com>
2024-05-14 09:55:27 +0200

    ft(DS I)Task 0: Print array integers

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        } else if (size == 0){
+            Console.WriteLine();
+            return [];
+        } else {
+            int[] myArray = new int[size];
+            for (int i = 0; i<size; i++) {
+                myArray[i] = i;
+                Console.Write(myArray[i] + " ");
+            }
+            Console.WriteLine();
+            return myArray;
+        }
+    }
+}
