fc4d364acaeeed41d2843b13d5c27d324f51269c
GasanaJr <gasanadj2003@gmail.com>
2024-05-16 11:59:37 +0200

    bug(task4): fix null and zero values

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -4,6 +4,13 @@ class List
 {
     public static List<int> CreatePrint(int size)
     {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0) {
+            Console.WriteLine();    
+        }
         List<int> list = new List<int>(size);
         for (int i = 0; i<size; i++)
         {
