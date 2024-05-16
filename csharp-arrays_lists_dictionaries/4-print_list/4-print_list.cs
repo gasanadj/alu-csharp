fa87a9ec1136be9c837d551872e3a1750571443c
GasanaJr <gasanadj2003@gmail.com>
2024-05-16 11:50:24 +0200

    ft(task4): print list size elements

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> list = new List<int>(size);
+        for (int i = 0; i<size; i++)
+        {
+            list.Add(i);
+        }
+        for(int j=0; j<size; j++) {
+            if (j ==  size - 1) {
+                Console.Write(list[j] + "\n");
+            }else {
+                Console.Write(list[j] + " ");
+            }
+        }
+        // foreach (int k in list) {
+        //     Console.Write(k);
+        // }
+        return list;
+    }
+}
