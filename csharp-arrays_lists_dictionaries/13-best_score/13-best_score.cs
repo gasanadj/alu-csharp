1a39b36883f43b40fb0732c638364f95619fbe8a
GasanaJr <gasanadj2003@gmail.com>
2024-05-21 00:01:13 +0200

    ft(task13): add best scoring

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myDict)
+    {
+        string bestScore = "";
+        int best = 0;
+        foreach(KeyValuePair<string,int> kvp in myDict)
+        {
+            if (kvp.Value > best)
+            {
+                bestScore = kvp.Key;
+                best = kvp.Value;
+            }else {
+                continue;
+            }
+        }
+        return bestScore;
+    }
+}
