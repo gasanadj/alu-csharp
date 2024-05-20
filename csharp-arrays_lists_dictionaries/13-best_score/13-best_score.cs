f99d64038e026253a53a01cfe81edecfde72ec0a
GasanaJr <gasanadj2003@gmail.com>
2024-05-21 00:12:20 +0200

    bug(fix): fix task 13

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -4,9 +4,14 @@
     {
         string bestScore = "";
         int best = 0;
+        if (myDict.Count == 0)
+        {
+            bestScore = "None";
+            return bestScore;
+        }
         foreach(KeyValuePair<string,int> kvp in myDict)
         {
-            if (kvp.Value > best)
+            if (kvp.Value >= best)
             {
                 bestScore = kvp.Key;
                 best = kvp.Value;
