c8c0ab7bf0efa47f1d6702e19ceb584f954b9ce5
GasanaJr <gasanadj2003@gmail.com>
2024-05-21 00:36:21 +0200

    ft(task15): add square matrix

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,28 +1,24 @@
 ﻿using System;
 
-class Array 
+public class Array
 {
     public static void Reverse(int[] array)
     {
-        if (array == null || array.Length == 0) 
+        if (array == null || array.Length == 0)
         {
             Console.WriteLine();
             return;
         }
-        int [] reverseArray = new int[array.Length];
-        int index = 0;
-        for(int i = array.Length - 1; i>=0; i--)
+
+        for (int i = array.Length - 1; i >= 0; i--)
         {
-            reverseArray[index] = array[i];
-            index++;
-        }
-        for (int j = 0; j<reverseArray.Length; j++) 
-        {
-            if (j == reverseArray.Length - 1){
-                Console.Write(reverseArray[j] + "\n");
-            }else {
-                Console.Write(reverseArray[j] + " ");
+            Console.Write(array[i]);
+            if (i > 0)
+            {
+                Console.Write(" ");
             }
         }
+
+        Console.WriteLine();
     }
 }
