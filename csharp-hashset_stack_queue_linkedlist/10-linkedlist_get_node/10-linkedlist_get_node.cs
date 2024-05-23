995f53e81e7241d1cc6feb72751a3f548f613c53
GasanaJr <gasanadj2003@gmail.com>
2024-05-23 11:24:16 +0200

    ft(task 10): add searching from index

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (!myLList.Contains(myLList.ElementAt(n)))
+        {
+            return 0;
+        }
+        return myLList.ElementAt(n);
+    }
+}
