cbee4ccf8dabe3320cfecfb8447351b51c7cf6bd
GasanaJr <gasanadj2003@gmail.com>
2024-05-23 12:25:51 +0200

    ft(csharp): add tasks 11,12, and 13

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,25 @@
+﻿class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+    #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        LinkedListNode<int> current = myLList.First;
+    #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+    #pragma warning disable CS8602 // Dereference of a possibly null reference.
+        while (current.Next != null) 
+        {
+                if (current.Value < n)
+                {
+                    current = current.Next;
+                }else {
+                    myLList.AddBefore(current, n);
+                    break;
+                }
+        }
+    #pragma warning restore CS8602 // Dereference of a possibly null reference.
+    #pragma warning disable CS8603 // Possible null reference return.
+        return myLList.Find(n);
+    #pragma warning restore CS8603 // Possible null reference return.
+    }
+
+}
---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,8 @@
+﻿class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int element = myLList.ElementAt(index);
+        myLList.Remove(element);
+    }
+}
