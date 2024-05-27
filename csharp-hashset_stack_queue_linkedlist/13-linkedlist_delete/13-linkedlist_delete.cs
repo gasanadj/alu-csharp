9224bf2cb1c137501b548bf7059df1f5f6494d88
GasanaJr <gasanadj2003@gmail.com>
2024-05-27 11:13:54 +0200

    bug(task 13): add index check

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -2,6 +2,9 @@
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
+        if (index > myLList.Count - 1 || index < 0){
+            return;
+        }
         int element = myLList.ElementAt(index);
         myLList.Remove(element);
     }
