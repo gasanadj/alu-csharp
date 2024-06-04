ddb15fab10b96fb9508a6337c36beec75f2b7d64
GasanaJr <gasanadj2003@gmail.com>
2024-06-04 11:13:43 +0200

    ft(exceptions): add safe printing

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,19 @@
+﻿class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        try
+        {
+            for (int i = 0; i<n; i++) {
+                count++;
+                Console.WriteLine(myList[i]);
+            }
+            return count;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return count-1;
+        }
+    }
+}
