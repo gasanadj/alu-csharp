a5119038bb5b847e5b1fc26a262137e11121d334
GasanaJr <gasanadj2003@gmail.com>
2024-06-04 16:00:51 +0200

    feat(exceptions): Lists Division

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> result = new List<int>();
+        int div = 0;
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                div = list1[i] / list2[i];
+                result.Add(div);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+            catch (DivideByZeroException)
+            {
+                result.Add(0);
+            }
+        }
+        return result;
+    }
+}
