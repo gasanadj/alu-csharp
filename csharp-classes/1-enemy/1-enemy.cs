4885f8e729454408076607d98391aae701fb0488
GasanaJr <gasanadj2003@gmail.com>
2024-06-10 10:57:35 +0200

    feat(classes): add constructor

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿namespace Enemies {
+    /// <summary>
+    /// This is a Class representation of a zombie
+    /// </summary>
+    class Zombie {
+        public int health;
+
+        /// <summary>
+        /// This is a constructor that sets the health to 0
+        /// </summary>
+
+        public Zombie() {
+            health = 0;
+        }
+    }
+}
