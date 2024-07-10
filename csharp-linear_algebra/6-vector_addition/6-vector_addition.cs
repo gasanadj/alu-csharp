6f215ad2f7a937b6885c3d7f1eb5ec43ff9f2ae9
GasanaJr <gasanadj2003@gmail.com>
2024-07-10 14:23:42 +0200

    feat(addition): add vector addition

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Headers;
+
+/// <summary>
+/// Vector math class
+/// </summary>
+class VectorMath {
+    /// <summary>
+    /// Method to add 2 vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns>vector</returns>
+    public static double[] Add(double[] vector1, double[] vector2) {
+
+        if (vector1.Length != vector2.Length || (vector1.Length !=2 && vector1.Length != 3)) {
+            return new double[] {-1}; 
+        }
+        double[] finalresult = new double[vector1.Length];
+        for(int i = 0; i<vector1.Length; i++) {
+            finalresult[i] = vector1[i] + vector2[i];
+        };
+        return finalresult;
+    }
+}
+
