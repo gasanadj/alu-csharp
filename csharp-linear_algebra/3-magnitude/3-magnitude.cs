058f7bc3b5ba7169661cfd912d8e72f396ff9f9c
GasanaJr <gasanadj2003@gmail.com>
2024-07-10 12:23:42 +0200

    feat(magnitude): add method to calculate length

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,15 @@
+﻿class VectorMath {
+    public static double Magnitude(double[] vector) {
+        if (vector.Length !=2 || vector.Length !=3) {
+            return -1;
+        }
+        double sumSquares = 0;
+        foreach(double el in vector) {
+            sumSquares += el * el;
+        }
+        double lenSquare = Math.Sqrt(sumSquares);
+        double len = Math.Round(lenSquare, 2);
+        return len;
+
+    }
+}
