9cc1bd4390b9863b76c43b2cca6bdc45a7b192fc
GasanaJr <gasanadj2003@gmail.com>
2024-07-10 14:35:45 +0200

    feat(multplication): add vector multiplication

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+/// <summary>
+/// Vector math class
+/// </summary>
+class VectorMath {
+    /// <summary>
+    /// This performs scalar multiplication
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <param name="scalar"></param>
+    /// <returns></returns>
+    public static double[] Multiply(double[] vector, double scalar) {
+        if (vector.Length !=2 && vector.Length !=3) {
+            return new double[] {-1};
+        }
+
+        double[] final  = new double[vector.Length];
+        for(int i = 0; i<vector.Length; i++) {
+            final[i] = vector[i] * scalar;
+        }
+        return final;
+
+    }
+}
+
