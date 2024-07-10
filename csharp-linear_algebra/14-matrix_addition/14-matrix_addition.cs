fa59f054b8805fd46e233644d49a42e98cbca8d4
GasanaJr <gasanadj2003@gmail.com>
2024-07-10 16:53:37 +0200

    feat(matrix): add matrix multiplication

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// This is the class responsible for matrix mathematics
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Public method to add 2 matrice.
+    /// </summary>
+    /// <param name="matrix1"></param>
+    /// <param name="matrix2"></param>
+    /// <returns></returns>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
+
+        if (rows1 != rows2 || cols1 != cols2)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        if (!((rows1 == 2 && cols1 == 2) || (rows1 == 3 && cols1 == 3)))
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] result = new double[rows1, cols1];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols1; j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return result;
+    }
+}
