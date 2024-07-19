cb0f36ac9de96f4a1ce58a873bcb3fefab0bfe31
GasanaJr <gasanadj2003@gmail.com>
2024-07-19 15:13:26 +0200

    feat(inverse): add 2D inverse matrix

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -9,28 +9,29 @@ class MatrixMath
     /// </summary>
     /// <param name="matrix"></param>
     /// <returns></returns>
-    public static double[,] Inverse2D(double[,] matrix)
+public static double[,] Inverse2D(double[,] matrix) {
 
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-        if (rows != 2 || cols != 2)
-        {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) {
             return new double[,] { { -1 } };
         }
-        double determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
 
-        if (determinant == 0)
-        {
+        double a = matrix[0, 0];
+        double b = matrix[0, 1];
+        double c = matrix[1, 0];
+        double d = matrix[1, 1];
+
+        double determinant = a * d - b * c;
+
+        if (determinant == 0) {
             return new double[,] { { -1 } };
         }
-        double[,] inverse = new double[2, 2];
 
-        inverse[0, 0] = Math.Round(matrix[1, 1] / determinant,2);
-        inverse[0, 1] = Math.Round(-matrix[0, 1] / determinant);
-        inverse[1, 0] = Math.Round(-matrix[1, 0] / determinant);
-        inverse[1, 1] = Math.Round(matrix[0, 0] / determinant);
+        double invDet = 1.0 / determinant;
 
-        return inverse;
+        return new double[,]
+        {
+            { Math.Round(invDet * d, 2), Math.Round(-invDet * b, 2) },
+            { Math.Round(-invDet * c, 2), Math.Round(invDet * a, 2) }
+        };
     }
 }
