59c578e001377306e8fdc0a9326e92f3eda4fffb
GasanaJr <gasanadj2003@gmail.com>
2024-07-19 11:45:57 +0200

    feat(matrix): add shear matrix 2D

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -11,43 +11,49 @@ class MatrixMath
     /// <param name="direction"></param>
     /// <param name="factor"></param>
     /// <returns></returns>
-    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
-    {
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor){
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
-        double[,] resultMatrix = new double[rows, cols];
-        if (rows != 2 || cols != 2)
-        {
-            return new double[,] { { -1 } };
+        double[,] result = new double[rows, cols];
+        double[,] shearMatrix = new double[rows, cols];
+
+        if(rows != 2 || cols != 2){
+            
+            return new double[,]{ {-1}};
         }
 
-        double[,] shearMatrix;
-        if (direction == 'x')
-        {
-            shearMatrix = new double[,] { { 1, factor }, { 0, 1 } };
+
+        if(direction != 'x' && direction != 'y'){
+           
+            return new double[,] { { -1}};
         }
-        else if (direction == 'y')
-        {
-            shearMatrix = new double[,] { { 1, 0 }, { factor, 1 } };
-        }
-        else
-        {
-            return new double[,] { { -1 } };
+        
+        if(direction == 'x'){
+            
+         shearMatrix = new double[,]{ 
+                { 1, 0},
+                { factor, 1}
+        };
         }
 
-        for (int i = 0; i < rows; i++)
-        {
+        if(direction == 'y'){
+             shearMatrix = new double[,]{
+                { 1, factor},
+                { 0, 1}
+            };
+        }
 
-            for (int j = 0; j < cols; j++)
-            {
+        for(int i = 0; i < rows; i++){
 
-                for (int k = 0; k < 2; k++)
-                {
-                    resultMatrix[i, j] += Math.Round(matrix[i, k] * shearMatrix[k, j], 2);
+                for(int j = 0; j < cols; j++){
+                
+                  for(int k = 0; k < 2; k++){
+                        result[i,j] += Math.Round(matrix[i, k] * shearMatrix[k, j], 2);
                 }
             }
         }
-        return resultMatrix;
+
+        return result;
 
     }
 }
