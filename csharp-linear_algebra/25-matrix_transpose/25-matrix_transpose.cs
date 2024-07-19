6f4f14cdb8a72129df4eaaff2994e96b568e90a7
GasanaJr <gasanadj2003@gmail.com>
2024-07-19 12:00:37 +0200

    feat(transpose): add the transpose of a matrix

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,27 @@
+﻿
+/// <summary>
+/// This is the class responsible for matrix mathematics
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Public method to perform the transpose of a matrix
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+public static double[,] Transpose(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double[,] resultMatrix = new double[cols, rows];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                resultMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        return resultMatrix;
+    }
+}
