b076d0f798900418f5e117331d06236fc7fb2bb0
GasanaJr <gasanadj2003@gmail.com>
2024-07-19 10:24:14 +0200

    feat(mult): add dimensionless matrix multiplication

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,41 @@
+﻿
+/// <summary>
+/// This is the class responsible for matrix mathematics
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Public method to perform multiplication between two matrices.
+    /// </summary>
+    /// <param name="matrix1"></param>
+    /// <param name="matrix2"></param>
+    /// <returns></returns>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
+
+        if (rows1 != cols2)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] result = new double[rows1, cols2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                double sum = 0;
+                for (int k = 0; k < cols1; k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                result[i, j] = sum;
+            }
+        }
+        return result;
+    }
+}
