229116f352ee107e3fd957bad3588d572c48d86d
GasanaJr <gasanadj2003@gmail.com>
2024-10-21 11:23:07 +0200

    feat(abstract): add abstract base class

---
+++ csharp-interfaces/0-abstract_thinking/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// Base Class
+/// </summary>
+public abstract class Base {
+    /// <summary>
+    /// The public property name
+    /// </summary>
+    public string? name;
+
+    /// <summary>
+    /// Override for the ToString()
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        Type type = name!.GetType();
+        return $"{name} is a {type}";
+    }
+}
