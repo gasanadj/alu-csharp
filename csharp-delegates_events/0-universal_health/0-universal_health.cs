7a8cc7908bb564dea7c269a7cf00e3ed0df9ff38
GasanaJr <gasanadj2003@gmail.com>
2024-10-25 10:49:14 +0200

    feat(player): create player class

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Player class
+/// </summary>
+public class Player {
+    /// <summary>
+    /// name property
+    /// </summary>
+    public string? name {get; set;}
+    /// <summary>
+    /// maxHp property
+    /// </summary>
+    public float? maxHp {get; set;}
+    /// <summary>
+    /// hp property
+    /// </summary>
+    public float? hp {get; set;}
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f) {
+        this.name = name;
+        if (maxHp > 0) {
+            this.maxHp = maxHp;
+        }else {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = maxHp;
+    }
+    /// <summary>
+    /// Method to Print the Health of the Player
+    /// </summary>
+    public void PrintHealth() {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+}
---
+++ csharp-delegates_events/README.md
@@ -0,0 +1,1 @@
+Csharp delegates and Events
